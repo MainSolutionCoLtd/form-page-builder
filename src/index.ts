9bedec9524329ec3126df3a2f184bed0227b730f
Sabal Poudel <sabalpoudel@gmail.com>
2026-08-25 12:08:02 +0545

    feat: add FormBuilderFeatures type and resolveFeatures utility

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ export type {
   SavedFormMeta, LocalizedString, LanguageOption, ChromeShape, StringsShape,
   InputField, TextareaField, SelectField, RadioField, CheckboxGroupField,
   CheckboxField, ToggleField, ParagraphField, ImageField, SubmitStyle,
-  SubmitPayload, FormBuilderHandle,
+  SubmitPayload, FormBuilderHandle, FormBuilderFeatures, ContentBlockType, InputFieldType,
 } from "./types";
